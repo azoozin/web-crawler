4fa14696ee2762767b3ff9d163d5c777bc1216da
azoozin <155941153+azoozin@users.noreply.github.com>
2024-11-16 11:43:38 -0300

    add test for url with https://www.

--- crawl.test.js
+++ crawl.test.js
@@ -2,9 +2,19 @@ import { test, expect } from "@jest/globals";
 import { normalizeURL } from "./crawl.js";
 
 // Test #1
+// check url has https://
 test("normalizeURL strip protocol", () => {
     const input = "https://blog.boot.dev/path";
     const actual = normizeURL(input);
     const expected = "blog.boot.dev/path";
+    expect(actual).toEqual(expected);
+});
+
+// Test #2
+// check for https://www. in url
+test("normalizeURL www.", () => {
+    const input = "https://www.blog.test.testing/path";
+    const actual = normalizeURL(input);
+    const expected = "blog.test.testing/path";
     expect(actual).toEqual(expected);
 });
